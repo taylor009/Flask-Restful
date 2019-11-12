96404565af528c5c48e9326fcdc8360f82f8c4a7
Taylor Gagne <tgagne@netsurion.com>
2019-11-12 11:51:09 -0500

    Optimized request parser making it reusable.

--- code/app.py
+++ code/app.py
@@ -14,6 +14,9 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="This field cannot be left blank")
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,7 +26,7 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -35,10 +38,7 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price', type=float, required=True, help="This field cannot be left blank")
-        data = parser.parse_args()
-
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
